68e42cea20f2c8a1535b7f33a176602c910583ce
Douglas Reis <doreis@lowrisc.org>
2025-06-25 15:15:43 +0100

    Add a simple test
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

---
+++ tests/test_rdl2ot.py
@@ -0,0 +1,32 @@
+import pytest
+from pathlib import Path
+import sys
+import subprocess
+
+CLI_TOOL_PATH = Path(__file__).parent.parent / "src/rdl2ot"
+SNAPSHOTS_DIR = Path(__file__).parent / "snapshots"
+
+def run_cli_tool(input_file_path: Path, output_dir_path: Path):
+    command = [
+        sys.executable,  # Use the current Python interpreter
+        str(CLI_TOOL_PATH),
+        'export-rtl', str(input_file_path), str(output_dir_path)
+    ]
+    result = subprocess.run(command, capture_output=True, text=True, check=False)
+    return result
+
+def test_cli_lc_ctrl(tmp_path: Path):
+    ip_block = "lc_ctrl"
+    for outfile in ["reg_pkg.sv", "reg_top.sv"]:
+        input_rdl = SNAPSHOTS_DIR / f"{ip_block}.rdl"
+        snapshot_file = (SNAPSHOTS_DIR / f"{ip_block}_{outfile}")
+        snapshot_content = snapshot_file.read_text(encoding='utf-8')
+        output_file = tmp_path / f"{ip_block}_{outfile}"
+        cli_result = run_cli_tool(input_rdl, tmp_path)
+        assert cli_result.returncode == 0, f"CLI exited with error: {cli_result.stderr}"
+        assert "Successfully finished!" in cli_result.stdout # Check for success message
+
+        actual_output_content = output_file.read_text(encoding='utf-8')
+
+        assert actual_output_content == snapshot_content, \
+        f"Output mismatch : Run `meld {snapshot_file} {output_file}`"
