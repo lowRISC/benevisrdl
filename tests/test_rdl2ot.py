c5281c1976026a4a6523f15508a18d1d07ddbe0e
Douglas Reis <doreis@lowrisc.org>
2025-07-04 17:20:52 +0100

    Autoformat using `ruff format`
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- tests/test_rdl2ot.py
+++ tests/test_rdl2ot.py
@@ -6,32 +6,39 @@ import subprocess
 CLI_TOOL_PATH = Path(__file__).parent.parent / "src/rdl2ot"
 SNAPSHOTS_DIR = Path(__file__).parent / "snapshots"
 
+
 def run_cli_tool(input_file_path: Path, output_dir_path: Path):
     command = [
         sys.executable,  # Use the current Python interpreter
         str(CLI_TOOL_PATH),
-        'export-rtl', str(input_file_path), str(output_dir_path)
+        "export-rtl",
+        str(input_file_path),
+        str(output_dir_path),
     ]
     result = subprocess.run(command, capture_output=True, text=True, check=False)
     return result
 
+
 def run_ip_test(tmp_path: Path, ip_block: str):
     for outfile in ["reg_pkg.sv", "reg_top.sv"]:
         input_rdl = SNAPSHOTS_DIR / f"{ip_block}.rdl"
-        snapshot_file = (SNAPSHOTS_DIR / f"{ip_block}_{outfile}")
-        snapshot_content = snapshot_file.read_text(encoding='utf-8')
+        snapshot_file = SNAPSHOTS_DIR / f"{ip_block}_{outfile}"
+        snapshot_content = snapshot_file.read_text(encoding="utf-8")
         output_file = tmp_path / f"{ip_block}_{outfile}"
         cli_result = run_cli_tool(input_rdl, tmp_path)
         assert cli_result.returncode == 0, f"CLI exited with error: {cli_result.stderr}"
-        assert "Successfully finished!" in cli_result.stdout # Check for success message
+        assert "Successfully finished!" in cli_result.stdout  # Check for success message
 
-        actual_output_content = output_file.read_text(encoding='utf-8')
+        actual_output_content = output_file.read_text(encoding="utf-8")
 
-        assert actual_output_content == snapshot_content, \
-                f"Output mismatch, to debug, run:\nmeld {output_file} {snapshot_file}\n"
+        assert actual_output_content == snapshot_content, (
+            f"Output mismatch, to debug, run:\nmeld {output_file} {snapshot_file}\n"
+        )
+
 
 def test_cli_lc_ctrl(tmp_path: Path):
     run_ip_test(tmp_path, "lc_ctrl")
+
 
 def test_cli_uart(tmp_path: Path):
     run_ip_test(tmp_path, "uart")
